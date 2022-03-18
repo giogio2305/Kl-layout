b7c761354f9fc3df040fad25cb779d7cd391370a
giogio2305 <dasilvagueye@gmail.com>
2022-03-18 02:54:59 +0100

    Kl-layout 18-03-22 02:54

--- pages/components/ChatBox.js
+++ pages/components/ChatBox.js
@@ -1,5 +1,5 @@
 import { PaperAirplaneIcon } from '@heroicons/react/outline'
-import { ChevronDownIcon,ChevronUpIcon,PlusIcon,XIcon } from '@heroicons/react/solid'
+import { ChevronDownIcon,ChevronUpIcon,PhoneIcon,PlusIcon,VideoCameraIcon,XIcon } from '@heroicons/react/solid'
 import React from 'react'
 
 function ChatBox({chatBox, chatReduce, setChatBox, setChatReduce}) {
@@ -11,33 +11,37 @@ function ChatBox({chatBox, chatReduce, setChatBox, setChatReduce}) {
                             {
                               chatReduce  
                               ?
-                              <div className='fixed flex flex-col items-center justify-start rounded shadow-lg bottom-0 right-24 mb-1 w-[338px] h-10 flex flex-col items-center bg-white'>
+                              <div className='fixed flex flex-col items-center justify-start rounded shadow-lg bottom-0 right-24 mb-1 w-[348px] h-10 flex flex-col items-center bg-white'>
         
                               <div className='flex items-center justify-start w-[100%] h-10 rounded bg-[#005b82] px-1'>
                                   
-                                  <div className='inline-flex items-center justify-center mr-20 ml-2'>
+                                  <div className='inline-flex items-center justify-center mr-12 ml-2'>
                                   <div  className='mx-2   d-flex justify-center items-center text-center  w-2 h-2 rounded-full bg-[#e80c00] cursor-pointer' ></div>
-                                      <div className='text-base text-white font-semibold w-[148px] truncate '>Cabraule Ketchanga</div>
+                                      <div className='text-[15.5px] text-white font-semibold w-[140px] truncate '>Cabraule Ketchanga</div>
                                   </div>
-                                  <div className='inline-flex items-center justify-center '>
-                              <ChevronUpIcon className='h-6 w-6 text-white cursor-pointer' onClick={()=>setChatReduce(!chatReduce)}/>
-                              <XIcon className='h-5 w-5 text-white ml-2 cursor-pointer' onClick={()=>setChatBox(!chatBox)}/>
+                                  <div className='inline-flex items-center justify-center mr-2'>
+                                  <ChevronUpIcon className='h-6 w-6 text-white cursor-pointer' onClick={()=>setChatReduce(!chatReduce)}/>
+                        <PhoneIcon className='h-5 w-5 text-white cursor-pointer ml-2' onClick={()=>setChatReduce(!chatReduce)}/>
+                        <VideoCameraIcon className='h-5 w-5 text-white cursor-pointer ml-2' onClick={()=>setChatReduce(!chatReduce)}/>
+                    <XIcon className='h-5 w-5 text-white ml-2 cursor-pointer' onClick={()=>setChatBox(!chatBox)}/>
                               </div>
                               
                               </div>
                               </div>
                               :
                                 
-                              <div className='fixed flex flex-col items-center justify-start rounded shadow-lg bottom-0 right-24 mb-1 w-[338px] h-[455px] flex flex-col items-center bg-white'>
+                              <div className='fixed flex flex-col items-center justify-start rounded shadow-lg bottom-0 right-24 mb-1 w-[348px] h-[455px] flex flex-col items-center bg-white'>
         
                     <div className='flex items-center justify-start w-[100%] h-10 rounded-t bg-[#005b82] px-1'>
                         
-                        <div className='inline-flex items-center justify-center mr-20 ml-2'>
+                        <div className='inline-flex items-center justify-center mr-12 ml-2'>
                         <div  className='mx-2   d-flex justify-center items-center text-center  w-2 h-2 rounded-full bg-[#e80c00] cursor-pointer' ></div>
-                            <div className='text-base text-white font-semibold w-[148px] truncate '>Cabraule Ketchanga</div>
+                            <div className='text-[15.5px] text-white font-semibold w-[140px] truncate '>Cabraule Ketchanga</div>
                         </div>
-                        <div className='inline-flex items-center justify-center '>
-                    <ChevronDownIcon className='h-6 w-6 text-white cursor-pointer' onClick={()=>setChatReduce(!chatReduce)}/>
+                        <div className='inline-flex items-center justify-center mr-2'>
+                        <ChevronDownIcon className='h-6 w-6 text-white cursor-pointer' onClick={()=>setChatReduce(!chatReduce)}/>
+                        <PhoneIcon className='h-5 w-5 text-white cursor-pointer ml-2' onClick={()=>setChatReduce(!chatReduce)}/>
+                        <VideoCameraIcon className='h-5 w-5 text-white cursor-pointer ml-2' onClick={()=>setChatReduce(!chatReduce)}/>
                     <XIcon className='h-5 w-5 text-white ml-2 cursor-pointer' onClick={()=>setChatBox(!chatBox)}/>
                     </div>
 
@@ -61,6 +65,8 @@ function ChatBox({chatBox, chatReduce, setChatBox, setChatReduce}) {
                             }
     </>
 }
+
+
 </>
   )
 }
